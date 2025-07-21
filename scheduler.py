6724cfb9789fe49c06cd2f89508e376954f6a732
Arnav Jain <22uec025@lnmiit.ac.in>
2025-07-21 17:48:29 +0530

    Add files via upload

---
+++ scheduler.py
@@ -0,0 +1,22 @@
+from main import app, update_all_station_wait_times, remove_completed_vehicles
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
+import time
+
+def start_scheduler():
+    print("Starting scheduler...")
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_all_station_wait_times, 'interval', minutes=1)
+    scheduler.add_job(remove_completed_vehicles, 'interval', minutes=1)
+    scheduler.start()
+    return scheduler
+
+if __name__ == "__main__":
+    with app.app_context():
+        scheduler = start_scheduler()
+        print("Scheduler is running...")
+        try:
+            while True:
+                time.sleep(1)
+        except (KeyboardInterrupt, SystemExit):
+            scheduler.shutdown()
